061ece659c98164305c13618e31b662c6e579351
Naoharu Sasaki <nsasaki@gitlab.com>
2023-06-02 07:51:08 +0900

    Created config package and write Mongo URL to .env file.

--- go-gin/main.go
+++ go-gin/main.go
@@ -5,43 +5,27 @@ import (
 	"encoding/json"
 	"fmt"
 	"go-gin/models"
-	"log"
 	"net/http"
-	"time"
+
+	"go-gin/configs"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
-
-var tutorialList = []models.Tutorial{
-	{Title: "Tutorial A", Description: "This is tutorial A", Published: false,
-		CreatedAt: "2023-05-01T02:53:48.690Z", UpdatedAt: "2023-05-01T02:53:48.690Z", ID: "644f29bc21eb646280e59c84"},
-	{Title: "Tutorial B", Description: "This is tutorial B", Published: false,
-		CreatedAt: "2023-05-01T02:53:48.690Z", UpdatedAt: "2023-05-01T02:53:48.690Z", ID: "644f29c2acefe3e62a866731"},
-	{Title: "Tutorial C", Description: "This is tutorial C", Published: false,
-		CreatedAt: "2023-05-01T02:53:48.690Z", UpdatedAt: "2023-05-01T02:53:48.690Z", ID: "644f2a0eacefe3e62a866735"},
-}
-
-var tutorialPostList = []models.Tutorial{
-	{Title: "Tutorial Post A", Description: "This is tutorial Post A", Published: false,
-		CreatedAt: "2023-05-01T02:53:48.690Z", UpdatedAt: "2023-05-01T02:53:48.690Z", ID: "1"},
-	{Title: "Tutorial Post B", Description: "This is tutorial Post B", Published: false,
-		CreatedAt: "2023-05-01T02:53:48.690Z", UpdatedAt: "2023-05-01T02:53:48.690Z", ID: "2"},
-	{Title: "Tutorial Post C", Description: "This is tutorial Post C", Published: false,
-		CreatedAt: "2023-05-01T02:53:48.690Z", UpdatedAt: "2023-05-01T02:53:48.690Z", ID: "3"},
-}
 
 func main() {
 
 	router := gin.Default()
-	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{
+	conf := cors.DefaultConfig()
+	conf.AllowOrigins = []string{
 		"http://localhost:8081",
 	}
-	router.Use(cors.New(config))
+
+	//run database
+	configs.ConnectDB()
+
+	router.Use(cors.New(conf))
 	router.GET("/api/tutorials", getAllTutorials)
 	router.PUT("/api/tutorials", insertTutorials)
 	router.POST("/api/tutorials", updateTutorials)
@@ -51,23 +35,7 @@ func main() {
 
 func getAllTutorials(c *gin.Context) {
 
-	credential := options.Credential{
-		AuthMechanism: "SCRAM-SHA-256",
-		AuthSource:    "admin",
-		Username:      "root",
-		Password:      "brHZ-!_rHAZF4xR2-EsRKx9e",
-	}
-
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:28001").SetAuth(credential))
-	if err != nil {
-		log.Fatal(err)
-	}
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	err = client.Connect(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	client := configs.ConnectDB()
 	collection := client.Database("tutorial").Collection("tutorial_collection")
 	filter := bson.D{{}}
 
@@ -82,6 +50,7 @@ func getAllTutorials(c *gin.Context) {
 		panic(err)
 	}
 
+	var tutorialList []models.Tutorial
 	for _, result := range results {
 		cursor.Decode(&result)
 		output, err := json.MarshalIndent(result, "", "    ")
@@ -91,10 +60,6 @@ func getAllTutorials(c *gin.Context) {
 		fmt.Printf("%s\n", output)
 		tutorialList = results
 	}
-
-	defer client.Disconnect(ctx)
-	println("main method")
-	println(tutorialList[0].Description)
 
 	c.IndentedJSON(http.StatusOK, tutorialList)
 	println("getAllTutorials")
